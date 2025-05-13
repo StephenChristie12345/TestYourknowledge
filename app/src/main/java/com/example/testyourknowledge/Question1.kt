6ecf36987ce11c75e7392f0d3ce3fd697a02d56e
Stephen Christie <ST10467438@vcconnect.edu.za>
2025-05-13 11:17:33 +0200

    started Coding the main activity

--- app/src/main/java/com/example/testyourknowledge/Question1.kt
+++ app/src/main/java/com/example/testyourknowledge/Question1.kt
@@ -1,6 +1,7 @@
 package com.example.testyourknowledge
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,19 @@ class Question1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_question1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonTrue = findViewById<Button>(R.id.True)
+        val buttonFalse = findViewById<Button>(R.id.False)
+
+        buttonTrue.setOnClickListener {
+            checkAnswer(true)
+        }
+        buttonFalse.setOnClickListener {
+            checkAnswer(false)
         }
     }
+}
+
+private fun Question1.checkAnswer(bool: Boolean) {}
+
 }
