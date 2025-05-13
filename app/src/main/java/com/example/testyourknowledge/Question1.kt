2ee68fdd9aa7dee0961338de3e454eeae15338f3
Stephen Christie <ST10467438@vcconnect.edu.za>
2025-05-13 17:58:22 +0200

    Coded question 1, copy and pasted for each question, going to edit them in my next commit

--- app/src/main/java/com/example/testyourknowledge/Question1.kt
+++ app/src/main/java/com/example/testyourknowledge/Question1.kt
@@ -1,7 +1,9 @@
 package com.example.testyourknowledge
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,7 @@ class Question1 : AppCompatActivity() {
 
         val buttonTrue = findViewById<Button>(R.id.True)
         val buttonFalse = findViewById<Button>(R.id.False)
+        val buttonNext = findViewById<Button>(R.id.Next)
 
         buttonTrue.setOnClickListener {
             checkAnswer(true)
@@ -22,9 +25,30 @@ class Question1 : AppCompatActivity() {
         buttonFalse.setOnClickListener {
             checkAnswer(false)
         }
+        buttonNext.setOnClickListener {
+            showNextQuestion()
+        }
+    }
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = getCorrectAnswerForCurrentQuestion()
+
+        if (userAnswer == correctAnswer) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Incoorect!", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun getCorrectAnswerForCurrentQuestion(): Boolean {
+        return true
+    }
+
+    private fun showNextQuestion() {
+        val intent = Intent(this, Question1::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
-private fun Question1.checkAnswer(bool: Boolean) {}
 
-}
+
+
--- app/src/main/java/com/example/testyourknowledge/Question3.kt
+++ app/src/main/java/com/example/testyourknowledge/Question3.kt
@@ -1,20 +1,50 @@
 package com.example.testyourknowledge
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Question3 : AppCompatActivity() {
+class Question1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_question3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_question1)
+
+        val buttonTrue = findViewById<Button>(R.id.True)
+        val buttonFalse = findViewById<Button>(R.id.False)
+        val buttonNext = findViewById<Button>(R.id.Next)
+
+        buttonTrue.setOnClickListener {
+            checkAnswer(true)
         }
+        buttonFalse.setOnClickListener {
+            checkAnswer(false)
+        }
+        buttonNext.setOnClickListener {
+            showNextQuestion()
+        }
+    }
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = getCorrectAnswerForCurrentQuestion()
+
+        if (userAnswer == correctAnswer) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Incoorect!", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun getCorrectAnswerForCurrentQuestion(): Boolean {
+        return true
+    }
+
+    private fun showNextQuestion() {
+        val intent = Intent(this, Question2::class.java)
+        startActivity(intent)
+        finish()
     }
 }
